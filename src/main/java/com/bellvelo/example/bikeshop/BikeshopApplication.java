ad52d507e9544fe6100d16061351ad810c284218
David Bell <dbelldev@icloud.com>
2019-01-16 14:59:43 +0000

    bike race team app started

---
+++ src/main/java/com/bellvelo/example/bikeshop/BikeshopApplication.java
@@ -0,0 +1,14 @@
+package com.bellvelo.example.bikeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeshopApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bellvelo/example/bikeshop/BikeshopApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bellvelo.example.bikeshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BikeshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
